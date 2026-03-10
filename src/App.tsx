867b7f28826b37a490a74d82ecfc80254c61b714
unknown <quocminh.vutran3105@gmail.com>
2026-03-10 09:29:35 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import NotFound from "./pages/NotFound";
 import { MainLayout } from "./layouts/MainLayout";
 import { baseRepoName } from "./shared/types";
 
-const base = baseRepoName
+const base = baseRepoName;
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<MainLayout />}>
-
-        <Route path={`${base}/`} element={<Home />} />
+          <Route path={`${base}/`} element={<Home />} />
           <Route path={`${base}/home/*`} element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Route>
-
-        
       </Routes>
     </>
   );
